1717498655ee902face14bbdc113627784baf4c5
Lynn Bradshaw <readyready15728@gmail.com>
2021-07-16 19:08:25 +0000

    Retiring from quitting

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function compute() {
     console.log(amount);
     console.log(year);
 
-    document.getElementById('result').innerHTML='If you deposit ' + principal + ',\<br\>at an interest rate of ' + rate + '%\<br\>You will receive an amount of ' + interest + ',\<br\>in the year ' + year + '\<br\>';
+    document.getElementById('result').innerHTML='If you deposit ' + principal + ',\<br\>at an interest rate of ' + rate + '%\<br\>You will receive an amount of ' + parseFloat(interest).toLocaleString() + ',\<br\>in the year ' + year + '\<br\>';
 }
 
 function updateRate() {
