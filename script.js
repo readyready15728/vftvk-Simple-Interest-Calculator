28fc15ee2a4716b67be52dbb2e3e02e9c3a4a87d
Lynn Bradshaw <readyready15728@gmail.com>
2021-07-16 18:43:38 +0000

    Fixing another JS bug

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function compute() {
     console.log(principal);
     console.log(rate);
     console.log(years);
-    console.log(interest);
+    console.log(amount);
     console.log(year);
 
     document.getElementById('result').innerHTML='If you deposit ' + principal + ',\<br\>at an interest rate of ' + rate + '%\<br\>You will receive an amount of ' + amount + ',\<br\>in the year ' + year + '\<br\>';
