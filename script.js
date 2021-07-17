68bd4a20578022b31272355d44e15d44d010e3f0
Lynn Bradshaw <readyready15728@gmail.com>
2021-07-17 19:14:01 +0000

    Removing console.log statements

--- script.js
+++ script.js
@@ -7,12 +7,6 @@ function compute() {
         var interest = principal * years * rate / 100;
         var year = new Date().getFullYear() + years;
 
-        console.log(principal);
-        console.log(rate);
-        console.log(years);
-        console.log(interest);
-        console.log(year);
-
         document.getElementById('result').innerHTML='If you deposit <mark>' + principal + '</mark>,\<br\>at an interest rate of <mark>' + rate + '%</mark>\<br\>You will receive an amount of <mark>' + parseFloat(interest).toLocaleString() + '</mark>,\<br\>in the year <mark>' + year + '</mark>\<br\>';
     } else {
         alert('Enter a positive number');
