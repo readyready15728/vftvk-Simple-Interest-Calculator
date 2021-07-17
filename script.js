fc838f7478cf59303eb5a3c89160b75d6d9e19e6
Lynn Bradshaw <readyready15728@gmail.com>
2021-07-17 19:12:32 +0000

    Adding form validation

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 function compute() {
     var principal = parseInt(document.getElementById('principal').value);
-    var rate = parseFloat(document.getElementById('rate').value);
-    var years = parseInt(document.getElementById('years').value);
-    var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear() + years;
+    
+    if (principal > 0) {
+        var rate = parseFloat(document.getElementById('rate').value);
+        var years = parseInt(document.getElementById('years').value);
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear() + years;
 
-    console.log(principal);
-    console.log(rate);
-    console.log(years);
-    console.log(interest);
-    console.log(year);
+        console.log(principal);
+        console.log(rate);
+        console.log(years);
+        console.log(interest);
+        console.log(year);
 
-    document.getElementById('result').innerHTML='If you deposit <mark>' + principal + '</mark>,\<br\>at an interest rate of <mark>' + rate + '%</mark>\<br\>You will receive an amount of <mark>' + parseFloat(interest).toLocaleString() + '</mark>,\<br\>in the year <mark>' + year + '</mark>\<br\>';
+        document.getElementById('result').innerHTML='If you deposit <mark>' + principal + '</mark>,\<br\>at an interest rate of <mark>' + rate + '%</mark>\<br\>You will receive an amount of <mark>' + parseFloat(interest).toLocaleString() + '</mark>,\<br\>in the year <mark>' + year + '</mark>\<br\>';
+    } else {
+        alert('Enter a positive number');
+        document.getElementById('principal').focus();
+    }
 }
 
 function updateRate() {
