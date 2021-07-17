fb089ccbefb9af589660799a5fff16bf8312f0b8
Lynn Bradshaw <readyready15728@gmail.com>
2021-07-17 19:00:18 +0000

    Adding highlighting of numbers

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function compute() {
     console.log(interest);
     console.log(year);
 
-    document.getElementById('result').innerHTML='If you deposit ' + principal + ',\<br\>at an interest rate of ' + rate + '%\<br\>You will receive an amount of ' + parseFloat(interest).toLocaleString() + ',\<br\>in the year ' + year + '\<br\>';
+    document.getElementById('result').innerHTML='If you deposit <mark>' + principal + '</mark>,\<br\>at an interest rate of <mark>' + rate + '%</mark>\<br\>You will receive an amount of <mark>' + parseFloat(interest).toLocaleString() + '</mark>,\<br\>in the year <mark>' + year + '</mark>\<br\>';
 }
 
 function updateRate() {
